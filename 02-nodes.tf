9f98e6dca4d1ae62a69f7416b9595efdaddebe27
Vitalii Koretskyi <wondertalik@gmail.com>
2023-04-22 14:27:28 +0200

    feat: add 80 port to worker firewall

--- 02-nodes.tf
+++ 02-nodes.tf
@@ -333,7 +333,17 @@ resource "hcloud_firewall" "firewall_workers" {
       "::/0"
     ]
   }
-    rule {
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = 80
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
     direction = "in"
     protocol  = "tcp"
     port      = 443
